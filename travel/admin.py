b592d0cbacb62cce8cccae40b58f44e7404a8e86
Bernd Zaiser <bernd@users.noreply.github.com>
2026-04-20 18:37:37 +0200

    admin: consistent user visibility and filtering across all models

--- travel/admin.py
+++ travel/admin.py
@@ -12,28 +12,52 @@ class TripAdmin(admin.ModelAdmin):
 
 @admin.register(Day)
 class DayAdmin(admin.ModelAdmin):
-    list_display = ('date', 'location', 'trip')
-    list_filter = ('trip', 'date')
+    list_display = ('date', 'get_user', 'location', 'trip')
+    list_filter = ('trip__user', 'date')
+    
+    def get_user(self, obj):
+        return obj.trip.user
+    get_user.short_description = _("Benutzer")
+    get_user.admin_order_field = 'trip__user'
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('title', 'type', 'day', 'time', 'cost_booked', 'is_paid')
-    list_filter = ('day__trip', 'type', 'is_paid')
+    list_display = ('title', 'get_user', 'type', 'day', 'time', 'cost_booked', 'is_paid')
+    list_filter = ('day__trip__user', 'type', 'is_paid')
     search_fields = ('title', 'notes')
+
+    def get_user(self, obj):
+        return obj.day.trip.user
+    get_user.short_description = _("Benutzer")
+    get_user.admin_order_field = 'day__trip__user'
 
 @admin.register(GlobalExpense)
 class GlobalExpenseAdmin(admin.ModelAdmin):
-    list_display = ('title', 'expense_type', 'trip', 'total_amount')
-    list_filter = ('trip', 'expense_type')
+    list_display = ('title', 'get_user', 'expense_type', 'trip', 'total_amount')
+    list_filter = ('trip__user', 'expense_type')
+
+    def get_user(self, obj):
+        return obj.trip.user
+    get_user.short_description = _("Benutzer")
+    get_user.admin_order_field = 'trip__user'
 
 @admin.register(DiaryEntry)
 class DiaryEntryAdmin(admin.ModelAdmin):
-    list_display = ('day',)
-    list_filter = ('day__trip',)
+    list_display = ('day', 'get_user')
+    list_filter = ('day__trip__user',)
+
+    def get_user(self, obj):
+        return obj.day.trip.user
+    get_user.short_description = _("Benutzer")
 
 @admin.register(DiaryImage)
 class DiaryImageAdmin(admin.ModelAdmin):
-    list_display = ('diary_entry', 'caption')
+    list_display = ('diary_entry', 'get_user', 'caption')
+    list_filter = ('diary_entry__day__trip__user',)
+
+    def get_user(self, obj):
+        return obj.diary_entry.day.trip.user
+    get_user.short_description = _("Benutzer")
 
 @admin.register(TripTemplate)
 class TripTemplateAdmin(admin.ModelAdmin):
@@ -52,5 +76,9 @@ class ChecklistTemplateAdmin(admin.ModelAdmin):
 
 @admin.register(ChecklistItemTemplate)
 class ChecklistItemTemplateAdmin(admin.ModelAdmin):
-    list_display = ('text', 'template', 'category')
+    list_display = ('text', 'get_user', 'template', 'category')
     list_filter = ('template__user', 'category')
+
+    def get_user(self, obj):
+        return obj.template.user
+    get_user.short_description = _("Benutzer")
