5cbd8801ed8b71405f51cc28d592f67c7947a1ec
Bernd Zaiser <bernd@users.noreply.github.com>
2026-04-23 11:46:59 +0200

    feat: enable media serving in production for NAS deployment

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 from pwa import views as pwa_views
@@ -14,9 +15,10 @@ urlpatterns = [
     path('serviceworker_v29.js', pwa_views.service_worker, name='serviceworker'),
     path('manifest.json', pwa_views.manifest, name='manifest'),
     path('', include('travel.urls')),
+    
+    # Ermöglicht das Laden von Bildern auch wenn DEBUG=False (wichtig für NAS-Betrieb)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
