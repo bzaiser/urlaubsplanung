afbd23aba69313b819094a76a9b12c380df954ee
Bernd Zaiser <bernd@users.noreply.github.com>
2026-04-23 08:31:20 +0200

    feat: make docker data volume and paths configurable via env

--- config/settings.py
+++ config/settings.py
@@ -99,7 +99,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = env('MEDIA_ROOT', default=os.path.join(BASE_DIR, 'media'))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
